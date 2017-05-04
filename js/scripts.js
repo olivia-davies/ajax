8b2249e876631890e65550d7dcad6af691ddd455
Olivia Davies <odavies@vcd-ceb131-19.eastern.ewu.edu>
2017-05-04 16:35:05 -0700

    Working on adding AirTable
    * added AirTable js
    * added UI Avatars

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
+// @codekit-prepend "airtable.js";
 
-console.log('Hello, World!');
+//console.log('Hello, World!');
+////$("#data").load('/data/data.txt');
 
-$("#data").load('/data/data.txt');
+
+//Configure/Handshake with AirTable
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyq0nsM8nVkKQneU'
+});
+var base = Airtable.base('appQO9Vc7jxoDG317');
